ed6c4d0ddd5c64e8c46663144b93103e683fab9d
John Oram <john@sendwithus.com>
2017-03-05 17:21:24 -0800

    use google api key

--- web/common/views/home.py
+++ web/common/views/home.py
@@ -1,5 +1,9 @@
+from django.conf import settings
 from django.shortcuts import render_to_response
 
 
 def home(request):
-    return render_to_response("home.html")
+    context = {
+        "GOOGLE_MAPS_API_KEY": settings.GOOGLE_MAPS_API_KEY,
+    }
+    return render_to_response("home.html", context)
