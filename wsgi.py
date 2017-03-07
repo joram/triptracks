ce991e0be74d540d6f88371776ba545a40947f31
John Oram <john@sendwithus.com>
2017-03-06 20:54:02 -0800

    change django interface

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trip-planner.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
 application = get_wsgi_application()
