f34f605b82ce528b0d7f2db4895efaf8de3e7e50
John Oram <john@sendwithus.com>
2017-03-08 17:57:09 -0800

    added scripts, changed google maps env key

--- web/settings/prod.py
+++ web/settings/prod.py
@@ -2,7 +2,7 @@ from base import *
 import dj_database_url
 
 SECRET_KEY = os.environ.get("TP_SECRET_KEY")
-GOOGLE_MAPS_API_KEY = os.environ.get("GOOGLE_MAPS_API_KEY")
+GOOGLE_MAPS_API_KEY = os.environ.get("TP_GOOGLE_MAPS_API_KEY")
 
 DATABASES = {'default': dj_database_url.config()}
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
