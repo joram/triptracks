6e8e6f48e8ba7b0f3b233caceab7e1289211aaca
John <john.c.oram@gmail.com>
2018-03-18 10:53:50 -0700

    prod settings

--- settings/prod.py
+++ settings/prod.py
@@ -1,8 +1,51 @@
 from settings.base import *
 import dj_database_url
 
-SECRET_KEY = os.environ.get("TP_SECRET_KEY")
+SECRET_KEY = os.environ.get("TP_SECRET_KEY", "youshouldntusethisoneinprod")
 GOOGLE_MAPS_API_KEY = os.environ.get("TP_GOOGLE_MAPS_API_KEY")
+GOOGLE_CLIENT_ID = os.environ.get("TP_GOOGLE_CLIENT_ID")
+CSRF_USE_SESSIONS = True
 
-DATABASES = {'default': dj_database_url.config()}
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'django.request': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
+}
+
+
+DATABASES = {
+    'default': dj_database_url.config(env="TP_DATABASE_URL")
+}
+DATABASES["default"]["NAME"] = DATABASES["default"]["NAME"].rstrip("\r")
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+
