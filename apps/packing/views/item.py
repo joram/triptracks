c4c48e4c2845cb5c72b9469772a469f1c19dc15a
John <john.c.oram@gmail.com>
2018-03-26 23:38:21 -0700

    more inclusive search

--- apps/packing/views/item.py
+++ apps/packing/views/item.py
@@ -10,13 +10,51 @@ def upload(request):
 
 def search(request):
     search_text = request.GET.get('text', "")
-    quantity = max(10, request.GET.get('quantity', 10))
-    if search_text == "":
-        qs = []
-    else:
-        qs = Item.objects.filter(name__icontains=search_text)[:quantity]
+    quantity = min(10, int(request.GET.get('quantity', '10')))
+    query_funcs = [
+        exact_match_name,
+        exact_match_description,
+        one_word_match_name,
+        one_word_match_description,
+    ]
+
     data = {
-        'count': len(qs),
-        'items': [item.json for item in qs]
+        'count': 0,
+        'items': []
     }
+
+    for func in query_funcs:
+        remaining = quantity - data['count']
+        if remaining <= 0:
+            break
+        items = func(search_text, remaining)
+        data["items"] += [item.json for item in items]
+        data["count"] += len(items)
+
     return JsonResponse(data)
+
+
+def exact_match_name(search_text, quantity):
+    return list(Item.objects.filter(name__icontains=search_text)[:quantity])
+
+
+def one_word_match_name(search_text, quantity):
+    search_words = [w for w in search_text.split(" ") if w != ""]
+    items = []
+    for word in search_words:
+        qs = Item.objects.filter(name__icontains=word)[:quantity-len(items)]
+        items += list(qs)
+    return items
+
+
+def exact_match_description(search_text, quantity):
+    return list(Item.objects.filter(description__icontains=search_text)[:quantity])
+
+
+def one_word_match_description(search_text, quantity):
+    search_words = [w for w in search_text.split(" ") if w != ""]
+    items = []
+    for word in search_words:
+        qs = Item.objects.filter(description__icontains=word)[:quantity-len(items)]
+        items += list(qs)
+    return items
