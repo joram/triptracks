f4ec8fe15d68d066eea3938be5f5a43f7332b021
John <john.c.oram@gmail.com>
2018-03-29 07:52:15 -0700

    bump

--- manage.py
+++ manage.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
     django.setup()
 
     from django.core.management.commands.runserver import Command as runserver
-    runserver.default_port = os.environ.get("PORT", "8000")
+    port = os.environ.get("PORT", "8000")
+    print "running on port {}".format(port)
+    runserver.default_port = port
 
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
