9fb034456ba86c60727eda8cda867bbdac5d7351
John <john.c.oram@gmail.com>
2018-03-30 01:49:22 -0700

    bump

--- settings/prod.py
+++ settings/prod.py
@@ -2,6 +2,9 @@ from settings.base import *
 import dj_database_url
 import os
 
+GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
+
 SECRET_KEY = os.environ.get("TP_SECRET_KEY", "youshouldntusethisoneinprod")
 GOOGLE_MAPS_API_KEY = os.environ.get("TP_GOOGLE_MAPS_API_KEY")
 GOOGLE_CLIENT_ID = os.environ.get("TP_GOOGLE_CLIENT_ID")
@@ -13,8 +16,6 @@ DATABASES["default"]["NAME"] = DATABASES["default"]["NAME"].rstrip("\r")
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 DATABASES['default']['OPTIONS'] = {'sslmode': 'require'}
 
-GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
-GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
 
 DEBUG = True
 
