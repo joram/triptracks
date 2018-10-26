09562da1af265a93d7fe88ce964a13c0ed48f835
John <john@sendwithus.com>
2018-10-26 15:24:23 -0700

    cycling generators working

--- apps/integrations/strava_worker.py
+++ apps/integrations/strava_worker.py
@@ -13,16 +13,28 @@ def pull_strava_activities(sleep_s):
                     "account": account,
                 }
 
-            for key in activity_generators:
-                activity_generator = activity_generators[key]["generator"]
-                account = activity_generators[key]["account"]
-                results = activity_generator.next()
-                if results is None:
-                    print "got no results"
-                    del activity_generators[key]
-                    continue
+            i = 0
 
-                activity, created = results
+            keys = activity_generators.keys()
+            while len(keys) > 0:
+                def get_results(i):
+                    if len(keys) == 0:
+                        return None, None, None
+                    key = keys[i % len(keys)]
+                    activity_generator = activity_generators[key]["generator"]
+                    account = activity_generators[key]["account"]
+                    try:
+                        results = activity_generator.next()
+                    except:
+                        keys.remove(key)
+                        return get_results(i)
+                    activity, created = results
+                    return account, activity, created
+
+                account, activity, created = get_results(i)
+                if account is None:
+                    break
+                i += 1
                 print "got activity:{activity_name} for account:{username} created:{created}".format(
                     activity_name=activity.route.name,
                     username=account.user.get_short_name(),
@@ -30,6 +42,7 @@ def pull_strava_activities(sleep_s):
                 )
                 time.sleep(sleep_s)
         except Exception as e:
+            print "error!"
             print e
 
         print "finished polling all accounts. sleeping for 10min"
