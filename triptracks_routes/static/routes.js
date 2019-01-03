f42944e173720f5b50faa4e12ac99ef7d10edc69
John <john.c.oram@gmail.com>
2019-01-02 21:20:35 -0800

    fixing first bit of scoping issues

--- triptracks_routes/static/routes.js
+++ triptracks_routes/static/routes.js
@@ -1,49 +1,52 @@
 class Route {
-    constructor(name, pub_id) {
-        this.name = name;
-        this.pub_id = pub_id;
-        this.lines = {};
-        this.map_lines = {};
-        this.curr_zoom = -1;
-    }
+  constructor(name, pub_id) {
+    this.name = name;
+    this.pub_id = pub_id;
+    this.lines = {};
+    this.map_lines = {};
+    this.curr_zoom = -1;
+    var self = this;
 
-    add_lines(lines, zoom) {
+    this.add_lines = function (lines, zoom) {
+        console.log("adding line ", this.pub_id, zoom, lines[0].length);
         this.lines[zoom] = lines;
 
 
-        this.map_lines[zoom] = [];
-        $.each(route.lines[zoom], function(i, coords){
-            latLngs = [];
-            $.each(coords, function (j, coord){
-              latLngs.push({lat: coord[0], lng: coord[1]})
-            });
-            line = new google.maps.Polyline({
-              path: latLngs,
-              geodesic: true,
-              strokeColor: '#FF0000',
-              strokeOpacity: 1.0,
-              strokeWeight: 2
-            });
-            line.setMap(map);
-            this.map_lines[zoom].push(line);
+        self.map_lines[zoom] = [];
+        $.each(lines, function (i, coords) {
+          var latLngs = [];
+          $.each(coords, function (j, coord) {
+            latLngs.push({lat: coord[0], lng: coord[1]})
+          });
+          var line = new google.maps.Polyline({
+            path: latLngs,
+            geodesic: true,
+            strokeColor: '#FF0000',
+            strokeOpacity: 1.0,
+            strokeWeight: 2
+          });
+          line.setMap(map);
+          self.map_lines[zoom].push(line);
         });
-    }
+      };
 
-    show(zoom){
-        if(zoom==this.curr_zoom) {
-            console.log("already showing ", this.pub_id, " at ", zoom);
-            return
+
+    this.show = function (zoom) {
+
+        if (zoom === this.curr_zoom) {
+          console.log("already showing ", this.pub_id, " at ", zoom);
+          return
         }
+        console.log("showing new route", this.lines.length, this.map_lines.length);
 
-        $.each(route.map_lines[zoom], function(i, line){
-            line.setMap(map);
+        $.each(self.map_lines[zoom], function (i, line) {
+          line.setMap(map);
         });
 
-        $.each(route.map_lines[this.curr_zoom], function(i, line){
-            line.setMap();
+        $.each(self.map_lines[this.curr_zoom], function (i, line) {
+          line.setMap();
         });
         this.curr_zoom = zoom;
+      }
     }
-
-
 }
--- triptracks_routes/static/routes_cache.js
+++ triptracks_routes/static/routes_cache.js
@@ -7,7 +7,7 @@ function geohashBounds(){
     for(i=0; i<h1.length; i++){
       prefix1 = h1.substring(0, i);
       prefix2 = h2.substring(0, i);
-        if(prefix1 != prefix2){
+        if(prefix1 !== prefix2){
             return h1.substring(0, i-1);
         }
     }
@@ -19,54 +19,57 @@ class RoutesCache {
     constructor() {
         this.fetched_hashes = {};
         this.routes = {};
-        this.curr_zoom = map.zoom;
-    }
+        this.curr_zoom = -1;
+        var self = this;
+        this.have = function (zoom, geohash){
+            if (!(geohash in this.fetched_hashes)){
+                return false;
+            }
+            return this.fetched_hashes[geohash].contains(map.zoom);
+        };
 
-    have(zoom, geohash){
-        if (!(geohash in this.fetched_hashes)){
-            return false;
+        this.update_zoom = function () {
+          $.each(this.routes, function(pub_id, route){
+            route.show(map.zoom);
+          });
+        };
+
+        this.update_bounds = function () {
+            console.log("updating bounds");
+            var geohash = geohashBounds();
+            if(self.have(map.zoom, geohash)){
+                console.log("already have ", geohash, zoom);
+                return false;
+            }
+            self.getRoutes();
+            return true;
+        };
+
+        this.getRoutes = function () {
+          console.log("getting routes");
+          var url = window.location.href;
+          if(!url.endsWith("/")){url += "/"}
+          url += "routes?bbox="+map.getBounds().toUrlValue()+"&zoom="+map.getZoom();
+          $.ajax({
+            url: url,
+            success: this.loadRoutes,
+          });
+        };
+
+        this.loadRoutes = function (data){
+            data = JSON.parse(data);
+            $.each(data.routes, function(pub_id, route){
+              self.loadRoute(pub_id, route, data.zoom);
+            });
+        };
+
+        this.loadRoute = function (pub_id, route, zoom){
+          if(!(pub_id in this.routes)){
+                this.routes[pub_id] = new Route(route.name, pub_id)
+          }
+          this.routes[pub_id].add_lines(route.lines, zoom);
+          this.routes[pub_id].show(zoom);
+          }
         }
-        return this.fetched_hashes[geohash].contains(map.zoom);
-    }
-
-    update_zoom() {
-      $.each(this.routes, function(pub_id, route){
-        route.show(map.zoom);
-      });
-    }
-
-    update_bounds() {
-        var geohash = geohashBounds();
-        if(this.have(map.zoom, geohash)){
-            console.log("already have ", geohash, zoom);
-            return false;
-        }
-        return true;
-    }
-
-    getRoutes() {
-	  url = window.location.href;
-	  if(!url.endsWith("/")){url += "/"}
-	  url += "routes?bbox="+map.getBounds().toUrlValue()+"&zoom="+map.getZoom();
-	  $.ajax({
-	    url: url,
-	    success: this.loadRoutes,
-	  });
-    }
-
-    loadRoutes(data){
-      data = JSON.parse(data);
-      $.each(data.routes, function(pub_id, route){
-        loadRoute(pub_id, route, data.zoom);
-      });
-      showRoutes();
-    }
-
-    loadRoute(pub_id, route, zoom){
-	  if(!(pub_id in this.routes)){
-          this.routes[pub_id] = new Route(route.name, pub_id)
-	  }
-	  this.routes[pub_id].add_lines(route.lines, zoom);
-    }
 
 }
