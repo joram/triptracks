4512b0374ee47cf7f5e3d9813f964833d5fac141
John <john@sendwithus.com>
2019-03-11 21:02:55 -0700

    include newrelic

--- server/settings/base.py
+++ server/settings/base.py
@@ -1,5 +1,7 @@
 from settings.default import *
 import os
+import newrelic.agent
+newrelic.agent.initialize('newrelic.ini')
 
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATICFILES_DIRS = (
